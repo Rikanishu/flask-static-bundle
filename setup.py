6efe28e368c3dae93db8e641eeac4494354d5e53
Rikanishu <adantess@gmail.com>
2014-07-28 16:42:30 +0400

    version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='flask-static-bundle',
-    version='0.1.0',
+    version='0.1.1',
     packages=['flask_static_bundle'],
     install_requires=['Flask', 'static-bundle'],
     author='Rikanishu',
